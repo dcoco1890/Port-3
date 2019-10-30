97820bae158dbde06e79baa0f46c08a6fccda1f6
Cohen <dcoco91@gmail.com>
2019-10-30 11:14:01 -0400

    had a loose closing div tag that was messing up the entire page. Took way longer than I'd like to admit to figure out the cause of the page breaking

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,28 @@
-$(document).ready(function() {
-    const $wrapper = $('.wrapper');
-    const up = $('.arrow#up');
-    const arrow = $('.arrow#down');
+$ (document).ready (function () {
+  const $wrapper = $ ('.wrapper');
+  const up = $ ('.arrow#up');
+  const arrow = $ ('.arrow#down');
 
-    up.on('click', () => fullpage_api.moveSectionUp());
-    arrow.on('click', () => {
-        fullpage_api.moveSectionDown();
-    });
+  up.on ('click', () => fullpage_api.moveSectionUp ());
+  arrow.on ('click', () => {
+    fullpage_api.moveSectionDown ();
+  });
 
-    $('#fullpage').fullpage({
-        //options here
-        autoScrolling: true,
-        scrollHorizontally: true,
-        scrollBar: true,
-        dragAndMove: true,
-        css3: true,
-        responsiveWidth: 900,
-        continuousVertical: true,
-        scrollOverflow: false,
-        navigation: true,
-        navigationPosition: 'left',
-        navigationTooltips: ['About', 'Portfolio', 'Find Me'],
+  $ ('#fullpage').fullpage ({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    css3: true,
+    responsiveWidth: 900,
+    scrollbar: true,
+    scrollOverflow: false,
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['About', 'Portfolio', 'Find Me'],
+    fixedElements: '.arrow#up, .arrow#down, #nav',
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+  });
 
-        anchors: ['firstPage', 'secondPage', '3rdPage'],
-    });
-
-    //methods
-    $.fn.fullpage.setAllowScrolling(true);
+  //methods
+  $.fn.fullpage.setAllowScrolling (true);
 });
